f0c41035c79cea9fef9d36842aabf9ec44780157
Ignacio <ignaxh@gmail.com>
2026-04-28 21:36:50 -0400

    Update earning calculation

--- calculate_earning/calculate_earning_tool.py
+++ calculate_earning/calculate_earning_tool.py
@@ -22,10 +22,11 @@ def set_tool(dolar_value):
 			input("\nDebe ingresar un valor númerico.")
 
 	amount_more_iva = ((iva_percentage / 100) * start_amount) + start_amount
-	earning_amount = ((earning_percentage / 100) * amount_more_iva) + amount_more_iva
+	#earning_amount = ((earning_percentage / 100) * amount_more_iva) + amount_more_iva
+	earning_amount = amount_more_iva / (1 - (earning_percentage / 100))
 
 	print("\n* Resultado de la suma de los porcentajes *")
-	print(f"\n| Monto inicial: {start_amount:.2f} | Monto con IVA: {amount_more_iva:.2f} | Monto con ganancia: {earning_amount:.2f} |");
+	print(f"\n| Monto inicial: {start_amount:.2f} | Monto con IVA: {amount_more_iva:.2f} | Monto con ganancia: {earning_amount:.2f} |")
 	print("\n* Resultado expresado en dolares *");
-	print(f"\n| Monto inicial: ${start_amount / dolar_value :.2f} | Monto con IVA: ${amount_more_iva / dolar_value :.2f} | Monto con ganancia: ${earning_amount / dolar_value :.2f} |");
-	input("\nPresione para continuar.");
+	print(f"\n| Monto inicial: ${start_amount / dolar_value :.2f} | Monto con IVA: ${amount_more_iva / dolar_value :.2f} | Monto con ganancia: ${earning_amount / dolar_value :.2f} |")
+	input("\nPresione para continuar.")
