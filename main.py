702e7eaecd85f38d0187abb1f5fa5cba2d62544a
Ignacio <ignaxh@gmail.com>
2026-05-11 19:30:46 -0400

    Create main window

--- main.py
+++ main.py
@@ -6,6 +6,26 @@ from calculate_earning import calculate_earning_tool
 from digital_weight import digital_weight_tool
 from dolar_value import dolar_value_tool
 
+import tkinter as tk
+
+window = tk.Tk()
+window.title("Store tools")
+window.geometry("400x300")
+window.resizable(False, False)
+
+top_menu = tk.Menu(window)
+window.config(menu=top_menu)
+
+tools_menu = tk.Menu(top_menu, tearoff=0)
+top_menu.add_cascade(label="Tools", menu=tools_menu)
+tools_menu.add_command(label="Calculate earning")
+tools_menu.add_command(label="Currency converter")
+tools_menu.add_command(label="Digital weight")
+
+config_menu = tk.Menu(top_menu, tearoff=0)
+top_menu.add_cascade(label="Config", menu=config_menu)
+config_menu.add_command(label="Update dolar value")
+
 # Get the dolar_value from the file.
 try:
 	with open("data/dolar_value.json", "r") as file_dolar_value:
@@ -24,6 +44,9 @@ except:
 		print("El archivo para valor del dólar ha sido creado")
 
 	dolar_value = data["dolar_value"]
+
+label_dolar_value = tk.Label(text=f"Dolar ($1): {dolar_value}")
+label_dolar_value.pack()
 
 # Show the main menu.
 while True:
@@ -44,3 +67,5 @@ while True:
 		dolar_value = dolar_value_tool.set_tool(dolar_value)
 	elif option == 5:
 		break
+
+window.mainloop()
